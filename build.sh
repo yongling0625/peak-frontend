ac8fba087d674ca538f9613138d7fa3a6b178812
zhuyl13 <0625@90z>
2020-03-29 20:51:08 +0800

    docker

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 yarn
 npm run build-prod
+
+if docker ps -a|grep -i peak-frontend_web;then
+   docker stop peak-frontend_web
+   echo '删除旧容器'
+   docker rm -f peak-frontend_web
+fi
+
 npm run docker-prod
